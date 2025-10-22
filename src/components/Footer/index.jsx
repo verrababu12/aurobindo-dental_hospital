c33b52f81af08190a738fa0b2f3179e23582b421
veeru <veeru@gmail.com>
2025-10-22 14:23:37 +0530

    footerstylesadded

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,6 +1,15 @@
 // Footer.jsx
 import "./index.css";
 import Search from "../Search";
+import { MdLocationPin } from "react-icons/md";
+import { BsFillTelephoneFill } from "react-icons/bs";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaTwitter,
+  FaLinkedin,
+} from "react-icons/fa";
+import { CgMail } from "react-icons/cg";
 
 const Footer = () => {
   return (
@@ -8,11 +17,48 @@ const Footer = () => {
       <div className="footer-contact">
         <h3>Contact Us</h3>
         <p>
+          <MdLocationPin className="contact-icon" />
           Flat no-503, 5th floor, MVS Heights, Vinayaka Nagar, Khanamet,
           Madhapur, Hyderabad-500081, Telangana.
         </p>
-        <p>Phone: 9866937777 / 9032018887 / 9347294612</p>
-        <p>Email: aurobindodental@gmail.com</p>
+        <p>
+          <BsFillTelephoneFill className="contact-icon" />
+          Phone: 9866937777 / 9032018887 / 9347294612
+        </p>
+        <p>
+          <CgMail className="contact-icon" /> Email: aurobindodental@gmail.com
+        </p>
+
+        <div className="footer-intro-social-icons">
+          <a
+            href="https://www.facebook.com/aurobindodental"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaFacebookF />
+          </a>
+          <a
+            href="https://www.instagram.com/aurobindodentalhospital/#"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaInstagram />
+          </a>
+          <a
+            href="https://twitter.com/dentalaurobindo"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaTwitter />
+          </a>
+          <a
+            href="https://www.linkedin.com/company/aurobindodentalhospital/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin />
+          </a>
+        </div>
       </div>
       <Search />
 
